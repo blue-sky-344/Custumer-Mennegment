4aa43bae0dad9b57c422f5433b6b1df315eea228
mr <marr328344@gmail.com>
2024-11-18 11:13:16 +0200

    add receipts component

---
+++ angular/src/app/components/receipts/receipts.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { ReceiptComponent } from "../receipt/receipt.component";
+import { Receipt } from '../../modules/receipt.interface';
+import { paymentType } from '../../modules/enums';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-receipts',
+  standalone: true,
+  imports: [ReceiptComponent, ReceiptComponent, CommonModule],
+  templateUrl: './receipts.component.html',
+  styleUrl: './receipts.component.scss'
+})
+export class ReceiptsComponent {
+  constructor(){
+  }
+  //template
+  receiptsToDisplay: Receipt[]=[{receiptNumber:1, userName:"aam334", ammount:50, date:new Date(), paymentMethod:paymentType.CASH, details:"notebooks"}]
+  
+}
