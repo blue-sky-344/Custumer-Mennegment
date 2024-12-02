023a5edc6c3304668948fc1e763c052f8b07acf5
baily <br166148@gmail.com>
2024-12-02 11:08:58 +0200

    fix appcomponn=ent again

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -3,28 +3,20 @@ import { RouterOutlet } from '@angular/router';
 import { ReceiptComponent } from "./components/receipt/receipt.component";
 import { Receipt } from './modules/receipt.interface';
 import { paymentType } from './modules/enums';
-<<<<<<< HEAD
-import {ExpensesComponent} from "./components/expenses/expenses.component"
+// import {ExpensesComponent} from "./components/expenses/expenses.component"
 import {OneExpenseComponent} from './components/one-expense/one-expense.component'
 import { ListExpensesComponent } from "./components/list-expenses/list-expenses.component";
-import { ReceiptsComponent } from "./components/receipts/receipts.component";
 
 
 
-=======
 import { ReceiptsComponent } from "./components/receipts/receipts.component";
 import { Observable } from 'rxjs';
 import { NewReceiptComponent } from "./components/new-receipt/new-receipt.component";
->>>>>>> 79b1f288d02cf18efe7fb9cfba5f0c8c1493cc68
 
 @Component({
   selector: 'app-root',
   standalone: true,
-<<<<<<< HEAD
-  imports: [RouterOutlet, ReceiptComponent, ExpensesComponent, OneExpenseComponent, ListExpensesComponent, ReceiptsComponent],
-=======
-  imports: [ReceiptComponent, ReceiptsComponent,NewReceiptComponent],
->>>>>>> 79b1f288d02cf18efe7fb9cfba5f0c8c1493cc68
+  imports: [RouterOutlet, ReceiptComponent, OneExpenseComponent, ListExpensesComponent, ReceiptsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
